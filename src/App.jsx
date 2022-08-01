2887fcec16aa1f3dc3962f8b48a2623e53876bd9
Arsen <Arcenk3x@mail.ru>
2022-08-01 22:32:23 +0500

    modal

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,36 @@ import Header from './components/header/Header';
 import Post from './components/post/Post';
 import { useEffect, useState } from 'react';
 import { db } from './firebase';
+import BasicModal from './components/modal/BasicModal';
 
 function App() {
 	const [posts, setPosts] = useState([])
+	const [open, setOpen] = useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
 
 	useEffect(() => {
 		db.collection('posts').onSnapshot(snapshot => {
-			setPosts(snapshot.docs.map(doc => doc.data()))
+			setPosts(snapshot.docs.map(doc => ({
+				id: doc.id,
+				post: doc.data()
+			})))
 		})
 	}, [])
 
 	return (
 		<div className="app">
 			<Header />
-			{posts.map(post =>
+			<BasicModal
+				handleOpen={handleOpen}
+				handleClose={handleClose}
+				open={open}
+			>
+				pmoddddal
+			</BasicModal>
+			{posts.map(({ id, post }) =>
 				<Post
-					key={post.id}
+					key={id}
 					username={post.username}
 					caption={post.caption}
 					imgUrl={post.imgUrl}
